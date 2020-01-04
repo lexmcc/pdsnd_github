be08b49cfc7199a93c9ec26664c23662490c6a37
lexmcc <lexmcconnell@gmail.com>
2020-01-04 17:27:44 +0000

    refactor: abstract birth_ stats into it's own function and call in user_stats

--- alex_bikeshare_2.py
+++ alex_bikeshare_2.py
@@ -312,6 +312,25 @@ def gender_stats(df):
         print('We don\'t seem to have any gender data for this data set.')
 
 
+def birth_stats(df):
+    '''Arguments: DataFrame.
+    Output: Display earliest, most recent, and most common year of birth if they exist in the DataFrame, else tell us there's no Birthday data.'''
+
+    if 'Birth Year' in df.columns:
+
+        earliest_dob = int(df['Birth Year'].min())
+        print('The earliest date of birth is: {}.'.format(earliest_dob))
+
+        most_recent_dob = int(df['Birth Year'].max())
+        print('The most recent date of birth is: {}.'.format(most_recent_dob))
+
+        most_common_dob = int(df['Birth Year'].value_counts().idxmax())
+        print('The most common date of birth is: {}.'.format(most_common_dob))
+
+    else:
+
+        print('We don\'t have any age related data for this data set.')
+
 
 def user_stats(df):
     """Displays statistics on bikeshare users."""
@@ -328,7 +347,6 @@ def user_stats(df):
         print("There are {} {}s.".format(user_types[user_type], user_type))
 
 
-
     # Display counts of gender
     ## If Gender not in the df, then say we have no gender data.
 
@@ -336,21 +354,7 @@ def user_stats(df):
 
     # Display earliest, most recent, and most common year of birth
 
-    if 'Birth Year' in df.index:
-
-        earliest_dob = int(df['Birth Year'].min())
-        print('The earliest date of birth is: {}.'.format(earliest_dob))
-
-        most_recent_dob = int(df['Birth Year'].max())
-        print('The most recent date of birth is: {}.'.format(most_recent_dob))
-
-        most_common_dob = int(df['Birth Year'].value_counts().idxmax())
-        print('The most common date of birth is: {}.'.format(most_common_dob))
-
-    else:
-
-        print('We don\'t have any age related data for this data set.')
-
+    birth_stats(df)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
